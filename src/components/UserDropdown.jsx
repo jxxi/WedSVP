999518909c74c1bf10f512e6c9de3de09032253c
Jasmine Wiggins <jasminerwiggins@gmail.com>
2020-07-09 23:42:50 -0600

    Add user  dropdown

--- src/components/UserDropdown.jsx
+++ src/components/UserDropdown.jsx
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownMenu from 'react-bootstrap/DropdownMenu';
+import DropdownItem from 'react-bootstrap/DropdownItem';
+import DropdownToggle from 'react-bootstrap/DropdownToggle';
+import { Link } from 'react-router-dom';
 
 class UserDropdown extends Component {
 	render() {
 		return (
-      <dropdown className="UserDropdown">
-        <toggle id="dropdown-basic">
-          <div className="User">
-            <div className="name">Sandra Dee</div>
-            <div className="image"><img src="http://68.media.tumblr.com/597a93f9a5d314cb01a9aebc2ae17216/tumblr_ntt5dof1oy1qadpa6o1_1280.jpg" alt="profile" /></div>
-          </div>
-        </toggle>
-
-        <dropdownMenu className="menu">
-          <dropdownItem href="#/action-1">Action</dropdownItem>
-          <dropdownItem href="#/action-2">Another action</dropdownItem>
-          <dropdownItem href="#/action-3">Something else</dropdownItem>
-        </dropdownMenu>
-      </dropdown>
+      <Dropdown className="UserDropdown">
+          <DropdownToggle className="dropdown">
+            <Link to='/my-profile'>
+              <div className="User">
+                <div className="name">Sandra Dee</div>
+                <div className="image"><img src="http://68.media.tumblr.com/597a93f9a5d314cb01a9aebc2ae17216/tumblr_ntt5dof1oy1qadpa6o1_1280.jpg" alt="profile" /></div>
+              </div>
+            </Link>
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem href="/my-profile">My Profile</DropdownItem>
+            <DropdownItem href="#">My Site</DropdownItem>
+            <DropdownItem href="#">Designer</DropdownItem>
+            <DropdownItem className="divider-before" href="#">Sign out</DropdownItem>
+          </DropdownMenu>
+      </Dropdown>
 		)
 	}
 }
